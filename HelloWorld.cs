2a3dee060413ef354a066fe1b55315780f976bb8
Smerga <smerrgamer@gmail.com>
2025-09-13 18:27:28 -0400

    This not the first commit of HelloWorld.cs sadly

---
+++ HelloWorld.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour 
+{
+     void Start() {
+        Debug.Log("Hello World");
+     }
+} 
